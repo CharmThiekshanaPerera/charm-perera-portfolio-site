7f8289029db8adb840d398d7a4192886e80c2a39
Thiekshana Perera <thiekshana@phyxle.com>
2025-11-13 19:04:25 +0530

    update @vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+// src/main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+// ADD THIS:
+import { injectSpeedInsights } from "@vercel/speed-insights";
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")!).render(<App />);
